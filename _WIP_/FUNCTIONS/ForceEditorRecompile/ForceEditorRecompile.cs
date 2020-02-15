c258281893df9b9bd0bb0bdc1db2bb8fff554596
Dave Mandrella <dx4dgames@gmail.com>
2020-02-15 01:27:57 -0500

    QOL: OpenMMO Menu - Build Changers added to Change Build subfolder QOL: Reorganized WIP Folders

--- _WIP_/FUNCTIONS/ForceEditorRecompile/ForceEditorRecompile.cs
+++ _WIP_/FUNCTIONS/ForceEditorRecompile/ForceEditorRecompile.cs
@@ -1,8 +1,12 @@
+#if UNITY_EDITOR
 using UnityEditor;
 
 public partial class Project
 {
-    public const string defineSymbol = "forceeditorrecompile";
+    public const string defineSymbol = "recompiling";
+
+    /// <summary>Force a recompile in the editor.
+    /// NOTE: This will trigger a rebuild twice...no way around it or it becomes unreliable. It works for now until Unity fixes the AssetDatabase.Refresh function.</summary>
     public static void ForceEditorRecompile()
     {
         BuildTargetGroup target = GetBuildTargetGroup();
@@ -10,14 +14,6 @@ public partial class Project
 
         PlayerSettings.SetScriptingDefineSymbolsForGroup(target, rawSymbols + defineSymbol);
         PlayerSettings.SetScriptingDefineSymbolsForGroup(target, rawSymbols);
-        /*if (rawSymbols.Contains(defineSymbol))
-        {
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(target, rawSymbols.TrimEnd(defineSymbol.ToCharArray()));
-        }
-        else
-        {
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(target, rawSymbols + defineSymbol);
-        }*/
     }
 
     public static BuildTargetGroup GetBuildTargetGroup()
@@ -25,3 +21,4 @@ public partial class Project
         return BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
     }
 }
+#endif
---
+++ _WIP_/MENUS/ChangeBuild/ChangeBuild.cs
@@ -0,0 +1,77 @@
+//BY DX4D
+#if UNITY_EDITOR
+using OpenMMO;
+using UnityEngine;
+using UnityEditor;
+using OpenMMO.Network;
+using OpenMMO.Portals;
+
+public class ChangeBuild
+{
+    //SERVER
+    [MenuItem("OpenMMO/Change Build/Server Mode")]
+    public static void SetServer()
+    {
+        Debug.Log("Please wait...");
+        PortalManager portal = GameObject.FindObjectOfType<PortalManager>();
+        if (portal)
+        {
+            portal.active = true;
+        }
+
+        ProjectConfigTemplate config = Resources.Load<ProjectConfigTemplate>("Configuration/ProjectConfiguration [Example]");
+        if (config)
+        {
+            config.networkType = NetworkType.Server;
+            Debug.Log("<b><color=yellow>[SERVER] Build Mode Activated</color>...please wait...</b>");
+        }
+
+        Project.ForceEditorRecompile();
+        //AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate); //NOTE: This is broken in Unity...we wrote our own function to replace it.
+    }
+
+    //CLIENT
+    [MenuItem("OpenMMO/Change Build/Client Mode")]
+    public static void SetClient()
+    {
+        Debug.Log("Please wait...");
+        PortalManager portal = GameObject.FindObjectOfType<PortalManager>();
+        if (portal)
+        {
+            portal.active = false;
+        }
+
+        ProjectConfigTemplate config = Resources.Load<ProjectConfigTemplate>("Configuration/ProjectConfiguration [Example]");
+        if (config)
+        {
+            config.networkType = NetworkType.Client;
+            Debug.Log("<b><color=blue>[CLIENT] Build Mode Activated</color>...please wait...</b>");
+        }
+
+        Project.ForceEditorRecompile();
+        //AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate); //NOTE: This is broken in Unity...we wrote our own function to replace it.
+    }
+
+    //HOST AND PLAY
+    [MenuItem("OpenMMO/Change Build/Host and Play Mode")]
+    public static void SetHostAndPlay()
+    {
+        Debug.Log("Please wait...");
+        PortalManager portal = GameObject.FindObjectOfType<PortalManager>();
+        if (portal)
+        {
+            portal.active = false;
+        }
+
+        ProjectConfigTemplate config = Resources.Load<ProjectConfigTemplate>("Configuration/ProjectConfiguration [Example]");
+        if (config)
+        {
+            config.networkType = NetworkType.HostAndPlay;
+            Debug.Log("<b><color=green>[HOST+PLAY] Build Mode Activated</color>...please wait...</b>");
+        }
+
+        Project.ForceEditorRecompile();
+        //AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate); //NOTE: This is broken in Unity...we wrote our own function to replace it.
+    }
+}
+#endif

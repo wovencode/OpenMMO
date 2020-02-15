f59ed409af2985d74a084e00e0fa7063cdc0da0f
Dave Mandrella <dx4dgames@gmail.com>
2020-02-15 03:49:30 -0500

    NEW: Extended Levels of Server Position Validation

--- Core/PlayerComponent/Scripts/Hooked/NetworkManager.PlayerComponent.cs
+++ Core/PlayerComponent/Scripts/Hooked/NetworkManager.PlayerComponent.cs
@@ -12,92 +12,128 @@ namespace OpenMMO.Network
 {
 
     // ===================================================================================
-	// NetworkManager
-	// ===================================================================================
-	public partial class NetworkManager
-	{
-				
-		protected List<GameObject> _playerPrefabs = null;
-		
-		// -------------------------------------------------------------------------------
-		// LoginPlayer_PlayerComponent
-		// -------------------------------------------------------------------------------
-		[DevExtMethods("LoginPlayer")]
-		public void LoginPlayer_PlayerComponent(NetworkConnection conn, GameObject player, GameObject prefab, string userName, string playerName)
-		{
-			player.GetComponent<PlayerComponent>().tablePlayer.Update(prefab, userName);
-		}
-		
-		// -------------------------------------------------------------------------------
-		// RegisterPlayer_PlayerComponent
-		// -------------------------------------------------------------------------------
-		[DevExtMethods("RegisterPlayer")]
-		public void RegisterPlayer_PlayerComponent(GameObject player, string userName, string prefabName)
-		{
-			player.transform.position = GetStartPosition(player).position;
-			player.GetComponent<PlayerComponent>().tablePlayer.Create(player, userName, prefabName);
-		}
-		
-		// ================================== PUBLIC =====================================
-		
-		// -------------------------------------------------------------------------------
-		// playerPrefabs
-		// -------------------------------------------------------------------------------
-		public List<GameObject> playerPrefabs
-		{
-			get
-			{
-				if (_playerPrefabs == null)
-					FilterPlayerPrefabs();
-				return _playerPrefabs;
-			}
-		}
+    // NetworkManager
+    // ===================================================================================
+    public partial class NetworkManager
+    {
 
-		// ================================== PROTECTED ==================================
-		
-		// -------------------------------------------------------------------------------
-		// GetStartPosition
-		// -------------------------------------------------------------------------------
-		protected Transform GetStartPosition(GameObject player)
-		{
-			
-			//TODO: Add start position randomization here
-			
-			foreach (Transform startPosition in startPositions)
-			{
-			
-				OpenMMO.Network.NetworkStartPosition position = startPosition.GetComponent<OpenMMO.Network.NetworkStartPosition>();
-				
-				if (position == null || position.archeTypes.Length == 0)
-					continue;
-				
-				PlayerComponent playerComponent = player.GetComponent<PlayerComponent>();
-				
-				foreach (ArchetypeTemplate template in position.archeTypes)
-					if (template == playerComponent.archeType)
-						return position.transform;
-						
-			}
-			
-			return GetStartPosition();
-		
-		}
-		
-		// -------------------------------------------------------------------------------
-		// ValidatePlayerPosition
-		// -------------------------------------------------------------------------------
-		protected void ValidatePlayerPosition(GameObject player)
-		{
+        protected List<GameObject> _playerPrefabs = null;
 
-			Transform transform = player.transform;
-			
-			if (!NavMesh.SamplePosition(player.transform.position, out NavMeshHit hit, 0.1f, NavMesh.AllAreas))
-				transform = GetStartPosition(player);
+        // -------------------------------------------------------------------------------
+        // LoginPlayer_PlayerComponent
+        // -------------------------------------------------------------------------------
+        [DevExtMethods("LoginPlayer")]
+        public void LoginPlayer_PlayerComponent(NetworkConnection conn, GameObject player, GameObject prefab, string userName, string playerName)
+        {
+            player.GetComponent<PlayerComponent>().tablePlayer.Update(prefab, userName);
+        }
 
-            if (transform != player.transform)
-			    player.GetComponent<PlayerComponent>().Warp(transform.position);
-			
-		}
+        // -------------------------------------------------------------------------------
+        // RegisterPlayer_PlayerComponent
+        // -------------------------------------------------------------------------------
+        [DevExtMethods("RegisterPlayer")]
+        public void RegisterPlayer_PlayerComponent(GameObject player, string userName, string prefabName)
+        {
+            player.transform.position = GetStartPosition(player).position;
+            player.GetComponent<PlayerComponent>().tablePlayer.Create(player, userName, prefabName);
+        }
+
+        // ================================== PUBLIC =====================================
+
+        // -------------------------------------------------------------------------------
+        // playerPrefabs
+        // -------------------------------------------------------------------------------
+        public List<GameObject> playerPrefabs
+        {
+            get
+            {
+                if (_playerPrefabs == null)
+                    FilterPlayerPrefabs();
+                return _playerPrefabs;
+            }
+        }
+
+        // ================================== PROTECTED ==================================
+
+        // -------------------------------------------------------------------------------
+        // GetStartPosition
+        // -------------------------------------------------------------------------------
+        protected Transform GetStartPosition(GameObject player)
+        {
+
+            //TODO: Add start position randomization here
+
+            foreach (Transform startPosition in startPositions)
+            {
+
+                OpenMMO.Network.NetworkStartPosition position = startPosition.GetComponent<OpenMMO.Network.NetworkStartPosition>();
+
+                if (position == null || position.archeTypes.Length == 0)
+                    continue;
+
+                PlayerComponent playerComponent = player.GetComponent<PlayerComponent>();
+
+                foreach (ArchetypeTemplate template in position.archeTypes)
+                    if (template == playerComponent.archeType)
+                        return position.transform;
+
+            }
+
+            return GetStartPosition();
+
+        }
+        public enum ValidationLevel { Complete, Tolerant, Low, None }
+        // -------------------------------------------------------------------------------
+        // ValidatePlayerPosition
+        // -------------------------------------------------------------------------------
+        [Tooltip("")]
+        [SerializeField] int tolerence = 5;
+        [SerializeField] ValidationLevel validation = ValidationLevel.Complete;
+        protected void ValidatePlayerPosition(GameObject player)
+        {
+            Transform transform = player.transform;
+
+            if (!NavMesh.SamplePosition(player.transform.position, out NavMeshHit hit, 0.1f, NavMesh.AllAreas))
+                transform = GetStartPosition(player);
+
+
+            if (ValidPosition(player.transform)) player.GetComponent<PlayerComponent>().Warp(transform.position);
+
+        }
+
+        bool ValidPosition(Transform playerTransform)
+        {
+            switch (validation)
+            {
+                case ValidationLevel.Complete:
+                    {
+                        return (transform != playerTransform);
+                    }
+                case ValidationLevel.Tolerant:
+                    {
+                        return (
+                            //X
+                            (transform.position.x >= playerTransform.position.x - tolerence
+                                && transform.position.x <= playerTransform.position.x + tolerence)
+                            //X
+                            && (transform.position.x >= playerTransform.position.x - tolerence
+                                && transform.position.x <= playerTransform.position.x + tolerence)
+                                //TODO TODO TODO
+                            && (transform.rotation != playerTransform.rotation)
+                            );
+                    }
+                case ValidationLevel.Low:
+                    {
+                        return (transform.position != playerTransform.position);
+                    }
+                case ValidationLevel.None:
+                    {
+                        return true;
+                    }
+            }
+
+            return false;
+        }
 		
 		// -------------------------------------------------------------------------------
 		// GetPlayerPrefab

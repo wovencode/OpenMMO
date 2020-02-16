6c24d022ec39fad1f11822dfcd912b818482403d
Dave Mandrella <dx4dgames@gmail.com>
2020-02-16 01:59:01 -0500

    REVERT: FaceTheCamera - Introduced new issues...will seek a different solution.

--- _WIP_/COMPONENTS/FaceTheCamera/FaceTheCamera.cs
+++ _WIP_/COMPONENTS/FaceTheCamera/FaceTheCamera.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 /// <summary>Turns this object to always face the camera.</summary>
 public class FaceTheCamera : MonoBehaviour
 {
-    [Header("ORIENTATION")]
-    [Tooltip("Always orient to the Vector3.up direction.")]
-    [SerializeField] bool alwaysPointUp = true;
-
 #pragma warning disable CS0414
     [Header("UPDATE FREQUENCY")]
     [Tooltip("How many update frames must pass before this component updates again?")]
@@ -26,8 +22,10 @@ public class FaceTheCamera : MonoBehaviour
         {
             frameCount = 0; //RESET THE COUNTER
 
-            if (Camera.main != null) gameObject.transform.forward = Camera.main.transform.forward; //FACE THE CAMERA
-            if (alwaysPointUp) gameObject.transform.up = Vector3.up; //POINT UP
+            if (Camera.main != null)
+            {
+                gameObject.transform.forward = Camera.main.transform.forward; //FACE THE CAMERA
+            }
         }
     }
 #endif

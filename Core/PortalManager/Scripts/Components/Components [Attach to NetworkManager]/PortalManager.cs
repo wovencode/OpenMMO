68e2eaa00d9549f82adb3c243cbed283e187cc36
Fhiz <support@wovencode.net>
2020-02-17 19:28:01 +0100

    Update PortalManager.cs

--- Core/PortalManager/Scripts/Components/Components [Attach to NetworkManager]/PortalManager.cs
+++ Core/PortalManager/Scripts/Components/Components [Attach to NetworkManager]/PortalManager.cs
@@ -205,7 +205,7 @@ namespace OpenMMO.Portals
     	// -------------------------------------------------------------------------------
 		protected void SpawnSubZone(int index)
 		{	
-			debug.Log("CALLED: SpawnSubZone");
+			debug.Log("CALLED: SpawnSubZone "+index);
 			Process process = new Process();
 			process.StartInfo.FileName 	= Tools.GetProcessPath;
 			process.StartInfo.Arguments = argZoneIndex + " " + index.ToString();

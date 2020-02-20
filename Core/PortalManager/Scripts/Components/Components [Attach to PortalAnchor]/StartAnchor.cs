fdf82446355bbc8bbb821d36952f1476ffb5e7b0
Fhiz <support@wovencode.net>
2020-02-20 18:15:26 +0100

    Create StartAnchor.cs

---
+++ Core/PortalManager/Scripts/Components/Components [Attach to PortalAnchor]/StartAnchor.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using OpenMMO;
+using OpenMMO.Network;
+using OpenMMO.Database;
+using OpenMMO.UI;
+using OpenMMO.DebugManager;
+using OpenMMO.Portals;
+
+namespace OpenMMO.Portals
+{
+
+	// ===================================================================================
+	// StartAnchor
+	// ===================================================================================
+	[DisallowMultipleComponent]
+	public class StartAnchor : MonoBehaviour
+	{
+		
+		[Tooltip("Add any number of archetypes")]
+		public ArchetypeTemplate[] archeTypes;
+		
+		// -------------------------------------------------------------------------------
+		// Awake
+		// -------------------------------------------------------------------------------
+        public void Awake()
+        {
+            AnchorManager.RegisterStartAnchor(this.gameObject);
+        }
+		
+		// -------------------------------------------------------------------------------
+		// OnDestroy
+		// -------------------------------------------------------------------------------
+        public void OnDestroy()
+        {
+            AnchorManager.UnRegisterStartAnchor(this.gameObject);
+        }
+    
+    	// -------------------------------------------------------------------------------
+    	
+	}
+
+}
+
+// =======================================================================================

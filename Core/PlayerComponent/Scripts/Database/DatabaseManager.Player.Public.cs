43715ac1e8433a96b2150ba313b3248bd29a186a
Fhiz <40817126+wovencode@users.noreply.github.com>
2020-02-20 13:03:20 +0100

    Update DatabaseManager.Player.Public.cs

--- Core/PlayerComponent/Scripts/Database/DatabaseManager.Player.Public.cs
+++ Core/PlayerComponent/Scripts/Database/DatabaseManager.Player.Public.cs
@@ -31,7 +31,7 @@ namespace OpenMMO.Database
 		{
 			TablePlayer tablePlayer = FindWithQuery<TablePlayer>("SELECT * FROM "+nameof(TablePlayer)+" WHERE playername=? AND banned=0 AND deleted=0", playerName);
 			
-			if (tablePlayer == null || (tablePlayer != null && tablePlayer.lastonline <= DateTime.MinValue))
+			if (tablePlayer == null)
 				return false;
 			else
 			{

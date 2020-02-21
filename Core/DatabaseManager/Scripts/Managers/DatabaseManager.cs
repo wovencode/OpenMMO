db76959f8953f4ae226c326436ef69bb8ad00d42
Fhiz <support@wovencode.net>
2020-02-21 17:52:44 +0100

    Update DatabaseManager.cs

--- Core/DatabaseManager/Scripts/Managers/DatabaseManager.cs
+++ Core/DatabaseManager/Scripts/Managers/DatabaseManager.cs
@@ -23,6 +23,8 @@ namespace OpenMMO.Database
 		public float saveInterval = 60f;
 		[Tooltip("Deleted user prune interval in seconds (0 to disable).")]
 		public float deleteInterval = 240f;
+		[Tooltip("Allow relogin after client inactivity in seconds (should be saveInterval or more).")]
+		public float logoutInterval = 90f;
 		
 		public static DatabaseManager singleton;
 		

4848948179d718dd1f3f1fdd0bdeac0b8f72bc17
Fhiz <support@wovencode.net>
2020-02-21 17:53:44 +0100

    Create Extensions.Mirror.cs

---
+++ Core/Tools/Scripts/Classes/Extensions.Mirror.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mirror;
+
+namespace Mirror
+{
+
+	// =======================================================================================
+	// Extensions
+	// =======================================================================================
+	public static partial class Extensions
+	{
+	
+		// -----------------------------------------------------------------------------------
+		// Id
+		// Returns the connection id of a NetworkConnection as formatted string
+		// used very often in logfiles
+		// Extends: NetworkConnection
+		// -----------------------------------------------------------------------------------
+		public static string Id(this NetworkConnection conn)
+		{
+				return "ID" + conn.connectionId.ToString();
+		}
+	
+		// -----------------------------------------------------------------------------------
+	
+	}
+
+}
+
+// =======================================================================================

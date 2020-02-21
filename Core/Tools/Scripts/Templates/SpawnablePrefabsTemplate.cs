5d2ca25ecb27b6f01fef33e334a10ad79c7dd395
Fhiz <support@wovencode.net>
2020-02-21 18:35:56 +0100

    Update SpawnablePrefabsTemplate.cs

--- Core/Tools/Scripts/Templates/SpawnablePrefabsTemplate.cs
+++ Core/Tools/Scripts/Templates/SpawnablePrefabsTemplate.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using OpenMMO;
-using OpenMMO.DebugManager;
+using OpenMMO.Debugging;
 using Mirror;
 #if UNITY_EDITOR
 using UnityEditor;

236b993b177397222f3e4b899e53f060a1504784
Fhiz <support@wovencode.net>
2020-02-21 18:35:39 +0100

    Update DebugProfile.cs

--- Core/Tools/Scripts/Managers/DebugManager/DebugProfile.cs
+++ Core/Tools/Scripts/Managers/DebugManager/DebugProfile.cs
@@ -1,12 +1,12 @@
 
 using OpenMMO;
-using OpenMMO.DebugManager;
+using OpenMMO.Debugging;
 using UnityEngine;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace OpenMMO.DebugManager
+namespace OpenMMO.Debugging
 {
 	
 	// ===================================================================================

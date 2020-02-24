5fc947eb5426d20fa74b27723bf3417538a2c273
Fhiz <support@wovencode.net>
2020-02-24 17:56:39 +0100

    Update NetworkManager.Events.cs

--- Core/NetworkManager/Scripts/Classes/NetworkManager.Events.cs
+++ Core/NetworkManager/Scripts/Classes/NetworkManager.Events.cs
@@ -7,30 +7,21 @@ using System;
 namespace OpenMMO.Network
 {
 
-    // -----------------------------------------------------------------------------------
-    // NetworkManager_Events
-    // -----------------------------------------------------------------------------------
+    // ===================================================================================
+	// NetworkManager_Events
+	// ===================================================================================
     /// <summary>
     /// Public partial class <c>NetworkManager_Events</c> that contains the network manager's events;
     /// </summary>
     [System.Serializable]
 	public partial class NetworkManager_Events
 	{
-		/// <summary>
-        /// Event called on server start
-        /// </summary>
+	
 		public UnityEvent OnStartServer;
-        /// <summary>
-        /// Event called on client start
-        /// </summary>
+		public UnityEvent OnStopServer;
 		public UnityEvent OnStartClient;
-        /// <summary>
-        /// Event called when a player succesfully logs in
-        /// </summary>
+		public UnityEvent OnStopClient;
 		public UnityEventConnection OnLoginPlayer;
-        /// <summary>
-        /// Event called when a player succesfully logs out
-        /// </summary>
 		public UnityEventConnection OnLogoutPlayer;
 		
 	}

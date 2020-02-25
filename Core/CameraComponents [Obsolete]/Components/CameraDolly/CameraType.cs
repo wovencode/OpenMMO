a05164d57cb3a5fedab175dec6f2acccf7c335cf
Fhiz <support@wovencode.net>
2020-02-25 18:58:05 +0100

    Removing Obsolete Scripts/Prefabs

--- Core/CameraComponents [Obsolete]/Components/CameraDolly/CameraType.cs
+++
@@ -1,6 +0,0 @@
-//BY DX4D
-/// <summary>
-/// New cameras can be added by putting the name of the camera in this enum 
-/// and then creating a new camera prefab with this name + the word Camera at the end.
-/// </summary>
-public enum CameraType { Malice, Fixed, Isometric, Orbit, FirstPerson, Tracking, Legacy }

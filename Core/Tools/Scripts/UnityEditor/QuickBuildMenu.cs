2b192a4d5b6a9fc8eb0c5b26fbb84aae414c6aa9
Dave Mandrella <dx4dgames@gmail.com>
2020-02-25 22:55:40 -0500

    UPDATE: QuickBuildMenu: SHOW_BUILD_REPORT define added to disable build reporting - may speed the builds up slightly, but there will be no feedback in the editor about the build process. (AKA: Advanced Mode)

--- Core/Tools/Scripts/UnityEditor/QuickBuildMenu.cs
+++ Core/Tools/Scripts/UnityEditor/QuickBuildMenu.cs
@@ -1,4 +1,5 @@
 //BY DX4D
+#define SHOW_BUILD_REPORT //Comment this out to turn off build reporting - May speed up build times slightly, but you will get no console feedback about your build.
 #if UNITY_EDITOR
 
 using OpenMMO;
@@ -27,11 +28,14 @@ namespace OpenMMO
             BuildTarget.StandaloneWindows64,
             BuildTarget.StandaloneOSX };
 
+        #region BUILD REPORT - build log
+#if SHOW_BUILD_REPORT
         static StringBuilder buildLog = new StringBuilder();
-
-		// -------------------------------------------------------------------------------
-		// GetScenesFromBuild
-		// -------------------------------------------------------------------------------
+#endif
+        #endregion
+        // -------------------------------------------------------------------------------
+        // GetScenesFromBuild
+        // -------------------------------------------------------------------------------
         public static string[] GetScenesFromBuild()
         {
             List<string> scenes = new List<string>();
@@ -85,8 +89,9 @@ namespace OpenMMO
             buildPlayerOptions.locationPathName = targetPlatform + "/" + "" + buildType + "." + buildFileExtension;
             buildPlayerOptions.target = targetPlatform;
             buildPlayerOptions.options = (headless) ? (BuildOptions.EnableHeadlessMode) : (BuildOptions.None);
- 
-            //BUILD LOG
+
+            #region  BUILD REPORT - header
+#if SHOW_BUILD_REPORT
             buildLog.AppendLine("<b>[BUILD] " + targetPlatform + " - " + ((headless) ? "(headless) " : "") + buildType + "</b>"
                 + "\n" + buildPlayerOptions.locationPathName);
 
@@ -94,6 +99,8 @@ namespace OpenMMO
             {
                 buildLog.AppendLine(scenePath);
             }
+#endif
+            #endregion
 
             //BUILD REPORTING
             BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
@@ -102,15 +109,23 @@ namespace OpenMMO
             //SUCCESS
             if (summary.result == BuildResult.Succeeded)
             {
+                #region  BUILD REPORT - success
+#if SHOW_BUILD_REPORT
                 buildLog.AppendLine("<color=green><b>" + targetPlatform + " " + buildType + " build succeeded..." + "</b></color>"
                     + "\nBuild size: " + summary.totalSize + " bytes"
                     + "\nBuild duration: " + System.Math.Round((summary.buildEndedAt - summary.buildStartedAt).TotalSeconds) + "s" );
+#endif
+                #endregion
             }
             //FAILURE
             if (summary.result == BuildResult.Failed)
             {
+                #region  BUILD REPORT - failure
+#if SHOW_BUILD_REPORT
                 buildLog.AppendLine("<color=red><b>" + targetPlatform + " " + buildType + " build failed...</b></color>"
                     + "\n" + report.ToString());
+#endif
+                #endregion
             }
         }
 
@@ -161,45 +176,77 @@ namespace OpenMMO
         [MenuItem("OpenMMO/Quick Build/FULL BUILD/Full Deployment", priority = 10)]
         public static void BuildFull()
         {
+            #region  BUILD REPORT - title
+#if SHOW_BUILD_REPORT
             buildLog.AppendLine("<color=orange><b>[BUILD REPORT]</b></color> " + "\n<b>Full Deployment Package</b>"); //BUILD REPORT
-            
+#endif
+            #endregion
+
             BuildFullDeployment(); //BUILD
 
+            #region  BUILD REPORT
+#if SHOW_BUILD_REPORT
             Debug.Log(buildLog.ToString()); //PRINT BUILD LOG
             buildLog.Clear(); //CLEAR BUILD LOG
+#endif
+            #endregion
         }
         //WINDOWS
         [MenuItem("OpenMMO/Quick Build/WIN64/Client and Server", priority = 11)]
         public static void BuildWindows64ClientAndServer()
         {
+            #region  BUILD REPORT - title
+#if SHOW_BUILD_REPORT
             buildLog.AppendLine("<color=orange><b>[BUILD REPORT]</b></color> " + "\n<b>Windows 64 Headless Server and Client</b>"); //BUILD REPORT
+#endif
+            #endregion
 
             BuildClientAndServer(BuildTarget.StandaloneWindows64); //BUILD
 
+            #region  BUILD REPORT
+#if SHOW_BUILD_REPORT
             Debug.Log(buildLog.ToString()); //PRINT BUILD LOG
             buildLog.Clear(); //CLEAR BUILD LOG
+#endif
+            #endregion
         }
         //MAC
         [MenuItem("OpenMMO/Quick Build/OSX/Client and Server", priority = 12)]
         public static void BuildMacClientAndServer()
         {
+            #region  BUILD REPORT - title
+#if SHOW_BUILD_REPORT
             buildLog.AppendLine("<color=orange><b>[BUILD REPORT]</b></color> " + "\n<b>OSX Headless Server and Client</b>"); //BUILD REPORT
-            
+#endif
+            #endregion
+
             BuildClientAndServer(BuildTarget.StandaloneOSX); //BUILD
-            
+
+            #region  BUILD REPORT
+#if SHOW_BUILD_REPORT
             Debug.Log(buildLog.ToString()); //PRINT BUILD LOG
             buildLog.Clear(); //CLEAR BUILD LOG
+#endif
+            #endregion
         }
         //LINUX
         [MenuItem("OpenMMO/Quick Build/Linux/Client and Server", priority = 13)]
         public static void BuildLinuxClientAndServer()
         {
+            #region  BUILD REPORT - title
+#if SHOW_BUILD_REPORT
             buildLog.AppendLine("<color=orange><b>[BUILD REPORT]</b></color> " + "\n<b>Linux Headless Server and Client</b>"); //BUILD REPORT
-            
+#endif
+            #endregion
+
             BuildClientAndServer(BuildTarget.StandaloneLinux64); //BUILD
 
+            #region  BUILD REPORT
+#if SHOW_BUILD_REPORT
             Debug.Log(buildLog.ToString()); //PRINT BUILD LOG
             buildLog.Clear(); //CLEAR BUILD LOG
+#endif
+            #endregion
         }
 
         //TODO: Android and iOS

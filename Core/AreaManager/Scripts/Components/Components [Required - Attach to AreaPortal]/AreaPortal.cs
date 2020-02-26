9304c63942422589622f5f25d95b09d7ccb047d8
Fhiz <support@wovencode.net>
2020-02-26 15:18:48 +0100

    Create AreaPortal.cs

---
+++ Core/AreaManager/Scripts/Components/Components [Required - Attach to AreaPortal]/AreaPortal.cs
@@ -0,0 +1,66 @@
+using OpenMMO;
+using OpenMMO.Debugging;
+using OpenMMO.Areas;
+using UnityEngine;
+using Mirror;
+
+namespace OpenMMO.Areas
+{
+    
+    // ===================================================================================
+	// AreaPortal
+	// ===================================================================================
+	[DisallowMultipleComponent]
+    public partial class AreaPortal: NetworkBehaviour
+    {
+    
+    	[Header("Sub Scene")]
+        [Tooltip("Assign the sub-scene(s) to load or unload for this area")]
+        public UnityScene subScene;
+       
+        // -------------------------------------------------------------------------------
+		// Awake
+		// -------------------------------------------------------------------------------
+        public void Awake()
+        {
+            AreaManager.singleton.RegisterAreaPortal(subScene);
+        }
+		
+		// -------------------------------------------------------------------------------
+		// OnDestroy
+		// -------------------------------------------------------------------------------
+        public void OnDestroy()
+        {
+        	if (AreaManager.singleton)
+            	AreaManager.singleton.UnRegisterAreaPortal(subScene);
+        }
+
+		// -------------------------------------------------------------------------------
+		// OnTriggerEnter
+		// @Server
+		// -------------------------------------------------------------------------------
+        [Server]
+        void OnTriggerEnter(Collider co)
+        {
+            NetworkIdentity ni = co.gameObject.GetComponentInParent<NetworkIdentity>();
+            AreaManager.singleton.LoadScenesAdditive(ni, subScene);
+        }
+        
+		// -------------------------------------------------------------------------------
+		// OnTriggerExit
+		// @Server
+		// -------------------------------------------------------------------------------
+        [Server]
+        void OnTriggerExit(Collider co)
+        {
+            NetworkIdentity ni = co.gameObject.GetComponentInParent<NetworkIdentity>();
+           	AreaManager.singleton.UnloadScenesAdditive(ni, subScene);
+        }
+        
+        // -------------------------------------------------------------------------------
+        
+    }
+    
+    // ===================================================================================
+    
+}

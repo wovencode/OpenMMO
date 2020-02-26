644bd49344f71c53d579b0214a95a988cc2e5066
Fhiz <support@wovencode.net>
2020-02-26 19:56:08 +0100

    Update NetworkManager.Client.Public.NetworkPortals.cs

--- Core/PortalManager/Scripts/Hooked/NetworkManager/NetworkManager.Client.Public.NetworkPortals.cs
+++ Core/PortalManager/Scripts/Hooked/NetworkManager/NetworkManager.Client.Public.NetworkPortals.cs
@@ -29,6 +29,7 @@ namespace OpenMMO.Network
         /// <param name="username"></param>
         public void TryAutoLoginPlayer(string playername, int token)
 		{
+Debug.Log("<<<<<<< TryAutoLoginPlayer");
 			RequestPlayerAutoLogin(NetworkClient.connection, playername, userName, token);
 		}
 

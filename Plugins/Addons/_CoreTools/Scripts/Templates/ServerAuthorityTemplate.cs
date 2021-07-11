fc3498acaf04e531877c3bccfdbb9a5e9c7ca061
Dave Mandrella <dx4dgames@gmail.com>
2021-07-11 16:53:03 -0700

    QoL: ServerAuthorityTemplate - Fixed formatting of inspector tooltip

--- Plugins/Addons/_CoreTools/Scripts/Templates/ServerAuthorityTemplate.cs
+++ Plugins/Addons/_CoreTools/Scripts/Templates/ServerAuthorityTemplate.cs
@@ -13,10 +13,10 @@ namespace OpenMMO
 	{
 	
 		[Header("SERVER SIDE AUTHORITY - movement")]
-		[Tooltip("The level of validation to player movement desired." +
-			"\nComplete: Validates the entire transform - Warps the player instantly back in position if they stray."
-			+ "Tolerant: Validates (only) positon with tolerance factored in - can return the player to the destired postion smoothly. - preferred"
-			+ "Low: Just validates position and nothing else.")]
+		[Tooltip("The level of validation to player movement desired."
+            + "\n" + " Complete - Validates the entire transform - Warps the player instantly back in position if they stray."
+			+ "\n" + " Tolerant - Validates (only) positon with tolerance factored in - can return the player to the destired postion smoothly. - preferred"
+			+ "\n" + " Low - Just validates position and nothing else.")]
 		[SerializeField] public ValidationLevel validation = ValidationLevel.Complete;
 		[Tooltip("When using Tolerant validation, a value will be tolerated if it is out of range by up to this amount in each direction.")]
 		[SerializeField] public float tolerence = 7f;
